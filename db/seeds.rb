2087ec1ca4ff43546a9cc0e3bfa39916d2891389
James Vasquez <jamesvas5307@live.com>
2017-07-19 08:31:38 -0400

    Checkpoint 20 Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 require 'random_data'
 
+50.times do
+  Question.create({
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    resolved: false
+  })
+end
+
+questions = Question.all
 
   50.times do
     Post.create({
@@ -22,3 +31,4 @@ require 'random_data'
   puts "Seed finished"
   puts "#{Post.count} posts created"
   puts "#{Comment.count} comments created"
+  puts "#{Question.count} comments created"
