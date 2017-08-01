49a39ae4f7bf19683154e0ef0e563dffcd2391e0
James Vasquez <jamesvas5307@live.com>
2017-08-01 14:01:51 -0400

    Checkpoint 22 Coursework

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,11 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
 
   def show
     @post = Post.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
 
@@ -16,12 +14,14 @@ class PostsController < ApplicationController
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
+    @topic = Topic.find(params[:topic_id])
+    @post.topic = @topic
 
     # if successful save Post to the database. And display a successful notice using flash.
     # Then redirect the user back to the post to show the user
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [ @topic, @post ]
     else
       # if no successful alert the user and render the new view again.
       flash.now[:alert] = "There was an error saving the post. Please try again"
@@ -40,7 +40,7 @@ class PostsController < ApplicationController
 
     if @post.save
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [ @post.topic, @post ]
     else
       flash.now[:alert] = "There was an error saving the post. Please try again"
       render :edit
@@ -51,7 +51,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-      redirect_to posts_path
+      redirect_to @post.topic
     else
       flash.now[:alert] = "There was an error deleting the post"
       render :show
