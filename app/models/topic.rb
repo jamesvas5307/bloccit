55debf28d0f5c869abbbb9fe5d11c3a35e46a64b
James Vasquez <jamesvas5307@live.com>
2017-08-01 19:21:40 -0400

    Another fix with merge conflict

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,9 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
-
   validates :name, length:{ minimum: 5 }, presence: true
   validates :description, length:{ minimum: 15 }, presence: true
   validates :public, presence: true
+
+  has_many :sponsoredposts, dependent: :destroy
+
 end
