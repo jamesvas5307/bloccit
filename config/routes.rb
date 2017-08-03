0f0bbb1cbb301e7fa9eba99dacf663548492cc74
James Vasquez <jamesvas5307@live.com>
2017-08-03 13:27:24 -0400

    Checkpoint 26 Coursework

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   resources :users, only:[:new, :create]
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   resources :topics do
     resources :posts, except: [:index]
     resources :sponsoredposts, except: [:index]
